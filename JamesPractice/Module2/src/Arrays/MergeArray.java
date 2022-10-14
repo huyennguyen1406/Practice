37d1181d1025d97489734707cd0509ee9a5ab7c8
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2022-10-14 23:34:06 +0700

    add merge arrays

--- JamesPractice/Module2/src/Arrays/MergeArray.java
+++ JamesPractice/Module2/src/Arrays/MergeArray.java
@@ -1,6 +1,5 @@
 package Arrays;
 
-import java.util.Arrays;
 
 public class MergeArray {
     public static void main(String[] args) {
@@ -8,15 +7,34 @@ public class MergeArray {
         int[] arr2 = {2, 4, 6, 8};
         int length = arr1.length + arr2.length;
         int[] merge = new int[length];
-        int num = 0;
-        for (int element : arr1) {
-            merge[num] = element;
-            num++;
+        int index = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            merge[index++] = arr1[i];
         }
-        for (int element : arr2) {
-            merge[num] = element;
-            num++;
+        for (int i = 0; i < arr2.length; i++) {
+            merge[index++] = arr2[i];
         }
-        System.out.println(Arrays.toString(merge));
+//        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < merge.length - 1; i++) {
+            System.out.print(merge[i] + ", ");
+        }
+        System.out.print(merge[merge.length - 1]);
+
+//        for (int num : merge) {
+//            s.append(num).append(
+//        String res = s.toString().substring(0, s.length() - 2);
+//        System.out.println(res);
+
+//        int num = 0;
+//        for (int element : arr1) {
+//            merge[num] = element;
+//            num++;
+//        }
+//        for (int element : arr2) {
+//            merge[num] = element;
+//            num++;
+//        }
+//        System.out.println(Arrays.toString(merge));
     }
 }
