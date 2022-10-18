ced5b86b5109919a81389cd7d6252154a800f223
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2022-10-19 00:02:20 +0700

    Total Number Column

---
+++ JamesPractice/Module2/src/Arrays/TotalNumberColumn.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class TotalNumberColumn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
