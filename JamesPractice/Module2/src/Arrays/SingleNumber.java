c97e12b87a75f859e0d0eb9d7d8dab882445d1a5
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2022-11-09 23:33:26 +0700

    add taotal number column

---
+++ JamesPractice/Module2/src/Arrays/SingleNumber.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 3, 3, 5, 6, 5, 7, 8, 7, 8, 2, 4};
+        System.out.println("Single element in array is: ");
+    }
+    public static int singleElement(int[] arr, int num) {
+        for (int i = 0; i < num; i++) {
+            int count = 0;
+            for (int j = 0; j < num; j++) {
+
+            }
+        }
+    }
+}
--- JamesPractice/Module2/src/Arrays/TotalNumberColumn.java
+++ JamesPractice/Module2/src/Arrays/TotalNumberColumn.java
@@ -5,6 +5,31 @@ import java.util.Scanner;
 public class TotalNumberColumn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.close();
+        System.out.println("Enter length array: ");
+        int length = sc.nextInt();
+        System.out.println("Enter width array: ");
+        int width = sc.nextInt();
+        int[][] arr = new int [length][width];
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.println("Element position [" + i + "][" + j + "] is: ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Enter column: ");
+        int col = sc.nextInt();
+        int total = 0;
+        for (int i = 0; i < length; i++) {
+            total += arr[i][col - 1];
+        }
+        System.out.println("Colum total is: " + total);
+        }
     }
-}
